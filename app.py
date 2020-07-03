09b84069cfd58d613cedab9e82b180a4f3f20847
EagleCoder404 <harsha.jediknight@gmail.com>
2020-07-03 14:03:26 +0530

    hahahahah

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or "sqlite:///test.db"
 db = SQLAlchemy(app)
 
 class posts(db.Model):
